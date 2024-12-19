0f930715f76849c814bae0a3be38c44abddde54b
maranix <ramanverma4183@gmail.com>
2024-12-19 11:43:10 +0530

    feat(monad): add flatten functionality

--- packages/tiny_fp/lib/src/monad.dart
+++ packages/tiny_fp/lib/src/monad.dart
@@ -10,9 +10,12 @@ import "package:tiny_fp/src/hkt.dart";
 /// - [T] is the type parameter
 abstract interface class Monad<C, T>
     implements Applicative<C, T>, Functor<C, T> {
-  /// Flattens the value inside the container using a function `f`.
+  /// Flattens the value inside the container using a function `f` on a single level.
   ///
   /// - [R] is the output type after transformation.
   /// - [f] is a function that maps the value inside the monad to another monad.
   HKT<C, R> flatMap<R>(HKT<C, R> Function(T) f);
+
+  /// Flattens nested values inside the container until a single level is remaining.
+  HKT<C, R> flatten<R>();
 }
